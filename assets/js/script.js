f654097457764f0f43d468e240d489be61ee93f0
Mathys Dezitter <145459988+Mathys97459@users.noreply.github.com>
2024-09-17 16:53:46 +0200

    pause/play svg

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,6 +37,7 @@ function afficherMusiques(musiques) {
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#FA64B2" class="bi bi-play-fill" viewBox="0 0 16 16">
                     <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
                 </svg>
+                
             </button>
         `;
         musicCard.appendChild(playDiv);
@@ -78,9 +79,9 @@ function playState(musique, id) {
         // Si c'est un nouvel audio, ou si la musique a été mise en pause, démarrer ou reprendre
         audio.play();
         playButton.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#FA64B2" class="bi bi-pause-fill" viewBox="0 0 16 16">
-                <path d="M5.5 3.5a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-1zm6 0a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h1z"/>
-            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-pause-fill" viewBox="0 0 16 16">
+  <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5"/>
+</svg>
         `;
         currentAudio = audio; // Mettre à jour l'audio en cours
 
