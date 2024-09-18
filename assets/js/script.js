c837de084a7fccf6ff640041075510f2f7af104d
Mathys Dezitter <145459988+Mathys97459@users.noreply.github.com>
2024-09-18 14:24:40 +0200

    fix bug merge jeudi 14h24

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,7 @@ window.onload = function () {
 };
 
 function afficherMusiques(musiques) {
+    console.log(musiques)
     musiques.forEach((musique) => {
         // Création de l'élément pour chaque carte de musique
         const musicCard = document.createElement("div");
@@ -50,19 +51,7 @@ function afficherMusiques(musiques) {
         <h3>${musique.title}</h3>
         <p>${musique.author}</p>
         `;
-    musicCard.appendChild(infoDiv);
-
-        // Ajout du bouton de lecture
-        const playDiv = document.createElement("div");
-        playDiv.classList.add("play");
-        playDiv.innerHTML = `
-        <button id="play-${musique.id}">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#FA64B2" class="bi bi-play-fill" viewBox="0 0 16 16">
-                <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
-            </svg>
-        </button>
-        `;
-        musicCard.appendChild(playDiv);
+        musicCard.appendChild(infoDiv);
 
         // Ajout de l'audio
         const audio = document.createElement("audio");
@@ -72,23 +61,24 @@ function afficherMusiques(musiques) {
         audio.style.display = "none"; // Cacher les éléments audio
         musicCard.appendChild(audio);
 
+        musicContainer.appendChild(musicCard);
         // Ajouter l'événement de clic au bouton de lecture
         musicCard.addEventListener("click", () =>
             afficherBanniere(musiques, musique, musique.id)
         );
 
-    // Ajout de l'événement 'ended'
-    audio.addEventListener("ended", () => {
-      const nextMusiqueId = parseInt(musique.id) + 1;
-      const nextAudio = document.getElementById(`audio-${nextMusiqueId}`);
-      // Vérifier si l'audio suivant existe
-      if (nextAudio) {
-        playState(musiques[nextMusiqueId], nextMusiqueId); // Passer à l'audio suivant
-      } else {
-        playState(musiques[0], 0);
-      }
+        // Ajout de l'événement 'ended'
+        audio.addEventListener("ended", () => {
+            const nextMusiqueId = parseInt(musique.id) + 1;
+            const nextAudio = document.getElementById(`audio-${nextMusiqueId}`);
+            // Vérifier si l'audio suivant existe
+            if (nextAudio) {
+                playState(musiques[nextMusiqueId], nextMusiqueId); // Passer à l'audio suivant
+            } else {
+                playState(musiques[0], 0);
+            }
+        });
     });
-  });
 }
 
 
@@ -115,85 +105,43 @@ function playState(musiques, musique, id) {
         currentAudio.currentTime = 0;
     }
 
-    // Vérifier si l'audio en question est en pause ou non
-    if (audio.paused) {
-        // Si c'est un nouvel audio, ou si la musique a été mise en pause, démarrer ou reprendre
-        audio.play();
-        playButton.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-pause-fill" viewBox="0 0 16 16">
-            <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5"/>
-            </svg>
-        `;
-        currentAudio = audio; // Mettre à jour l'audio en cours
-        console.log(audio.currentTime, audio.duration);
-        if (audio.currentTime >= audio.duration) {
-            id += 1;
-        } else {
-            audio.addEventListener("timeupdate", () => updateProgressBar(audio));
-        }
-
-        // Initialiser le contexte audio et le visualiseur pour cette musique
-        if (!audioCtx) {
-            audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-        }
         // Vérifier si l'audio en question est en pause ou non
         if (audio.paused) {
             // Si c'est un nouvel audio, ou si la musique a été mise en pause, démarrer ou reprendre
             audio.play();
-            btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"  fill="#FA64B2" class="bi bi-play-fill" viewBox="0 0 16 16"> <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" /> </svg>';
+            btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#FA64B2" class="bi bi-pause-fill" viewBox="0 0 16 16"><path d="M5.5 3.5a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h1zm6 0a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h1z"/></svg>`;
             currentAudio = audio; // Mettre à jour l'audio en cours
             audio.addEventListener('timeupdate', () => updateProgressBar(audio));
             // Initialiser le contexte audio et le visualiseur pour cette musique
             if (!audioCtx) {
                 audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-            }
-
-            // Vérifier si une source MediaElementSource a déjà été créée pour cet audio
-            if (!audio.source) {
-                audio.source = audioCtx.createMediaElementSource(audio);
-            }
-
-            // Créer un analyseur seulement si nécessaire
-            if (!analyser) {
                 analyser = audioCtx.createAnalyser();
                 analyser.fftSize = 512;
                 bufferLength = analyser.frequencyBinCount;
                 dataArray = new Uint8Array(bufferLength);
             }
-
-            // Connecter la source à l'analyseur et à la sortie audio
+            
+            if (!audio.source) {
+                audio.source = audioCtx.createMediaElementSource(audio);
+            }
+            
+            // Connecter la source à l'analyseur
             audio.source.connect(analyser);
             analyser.connect(audioCtx.destination);
-            afficherBanniere(musique);
-            // Visualiser les données
+            
+            // Commencer la visualisation
             frequenciesVisualizer();
-        } else {
-            // Si l'audio est en cours de lecture, le mettre en pause
-            audio.pause();
-            playButton.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#FA64B2" class="bi bi-play-fill" viewBox="0 0 16 16">
-                <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
-            </svg>
-        `;
-            currentAudio = null; // Réinitialiser l'audio en cours
-        }
-        // Connecter la source à l'analyseur et à la sortie audio
-        audio.source.connect(analyser);
-        analyser.connect(audioCtx.destination);
-
-        // Visualiser les données
-        frequenciesVisualizer();
-    } else {
+        }            
+    else {
         audio.pause();
         console.log(btn)
-        btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#FA64B2" class="bi bi-pause-fill" viewBox="0 0 16 16"><path d="M5.5 3.5a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h1zm6 0a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h1z"/></svg>';
+        btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#FA64B2" class="bi bi-play-fill" viewBox="0 0 16 16"> <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" /> </svg>';
         currentAudio = null; // Réinitialiser l'audio en cours
     }
 }
-}
 
 
-function afficherBanniere(musiques, musique) {
+function afficherBanniere(musiques, musique, id) {
     console.log("Afficher banniere id musique : " + musique.id);
     const banner = document.getElementById("musicBanner");
     const img = document.getElementById("banner-img");
@@ -237,32 +185,13 @@ function afficherBanniere(musiques, musique) {
     const btnPrevious = document.createElement("button");
     btnPlay.classList.add("banner-play");
     btnPlay.id = "play-" + musique.id; // Définir l'ID
-    btnPlay.innerHTML = `
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="30"
-    height="30"
-    fill="#FA64B2"
-    class="bi bi-play-fill"
-    viewBox="0 0 16 16"
-  >
-    <path
-      d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"
-    />
-  </svg>
-`;
+    btnPlay.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#FA64B2" class="bi bi-pause-fill" viewBox="0 0 16 16"><path d="M5.5 3.5a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h1zm6 0a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h1z"/></svg>`;
+
     btnNext.classList.add("banner-next");
     btnNext.id = "next-" + musique.id; // Définir l'ID
     btnNext.innerHTML = `
-  <svg xmlns="http://www.w3.org/2000/svg " 
-    width="30"
-    height="30"
-    fill="#FA64B2"
-    viewBox="0 0 43.25 45.39">
-<path fill="#fa64b2" d="M3.89,45.39h-.89c-1.41-.29-2.3-1.16-2.68-2.52-.19-.66-.31-1.36-.31-2.04C0,28.75,0,16.66,0,4.58c0-.87.12-1.73.46-2.55C1.03.67,2.3-.08,3.74.07c1.1.11,2.02.61,2.9,1.22,8.26,5.68,16.52,11.36,24.77,17.04.44.3.87.63,1.24,1,1.4,1.43,1.91,3.1,1.18,5.02-.46,1.21-1.37,2.04-2.41,2.76-8.09,5.56-16.18,11.13-24.26,16.7-1.01.7-2.04,1.35-3.28,1.58Z"/>
-<path fill="#fa64b2" d="M39.53,45.39c-.41-.16-.85-.26-1.21-.5-.79-.52-1.1-1.3-1.1-2.24,0-5.8,0-11.61,0-17.41,0-7.46.03-14.92-.02-22.37-.01-1.81,1.3-2.96,2.86-2.86.13,0,.27,0,.4,0,1.79,0,2.78.97,2.78,2.76,0,13.28,0,26.55,0,39.83,0,1.54-.57,2.29-2.12,2.79h-1.6Z"/>
-</svg>
-`;
+  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#FA64B2" viewBox="0 0 43.25 45.39"> <path fill="#fa64b2" d="M3.89,45.39h-.89c-1.41-.29-2.3-1.16-2.68-2.52-.19-.66-.31-1.36-.31-2.04C0,28.75,0,16.66,0,4.58c0-.87.12-1.73.46-2.55C1.03.67,2.3-.08,3.74.07c1.1.11,2.02.61,2.9,1.22,8.26,5.68,16.52,11.36,24.77,17.04.44.3.87.63,1.24,1,1.4,1.43,1.91,3.1,1.18,5.02-.46,1.21-1.37,2.04-2.41,2.76-8.09,5.56-16.18,11.13-24.26,16.7-1.01.7-2.04,1.35-3.28,1.58Z"/> <path fill="#fa64b2" d="M39.53,45.39c-.41-.16-.85-.26-1.21-.5-.79-.52-1.1-1.3-1.1-2.24,0-5.8,0-11.61,0-17.41,0-7.46.03-14.92-.02-22.37-.01-1.81,1.3-2.96,2.86-2.86.13,0,.27,0,.4,0,1.79,0,2.78.97,2.78,2.76,0,13.28,0,26.55,0,39.83,0,1.54-.57,2.29-2.12,2.79h-1.6Z"/> </svg> `;
+
     btnPrevious.classList.add("banner-previous");
     btnPrevious.id = "prev-" + musique.id; // Définir l'ID
     btnPrevious.innerHTML = `
